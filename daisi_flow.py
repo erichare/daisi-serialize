55a926e9c0b219c4ebb43b97639182c06f4b9563
Eric Hare <eric@omnianalytics.io>
2022-02-28 14:00:56 -0700

    Update daisi_flow.py

--- daisi_flow.py
+++ daisi_flow.py
@@ -1,6 +1,21 @@
 from pydaisi import Daisi
 import numpy as np
-from map_stack import MapStack
+
+class MapStack:
+    def __init__(self, nx, ny):
+        self.nx = nx
+        self.ny = ny
+        self.nb_layers = None
+        self.maps = []
+
+    def add_layer(self, map):
+        if len(map.shape) == 2 and map.shape[0] == self.ny and map.shape[1] == self.nx:
+            self.maps.append(map)
+            self.nb_layers = len(self.maps)
+            return "Map sucessfully added."
+        else:
+            return "Could not add map. Incompatible dimensions."
+
 
 obj_daisi = Daisi("Daisi Serialize")
 
@@ -10,7 +25,7 @@ ms = MapStack(nx, ny)
 for i in range(10):
     ms.add_layer(np.random.rand(nx, ny))
 
-result = obj_daisi.compute(func="compute", map_stack=ms, map=np.random.rand(nx, ny))
+status, ms = obj_daisi.compute(func="compute", map_stack=ms, map=np.random.rand(nx, ny))[0]
 
 print(status, ms.nb_layers)
 
