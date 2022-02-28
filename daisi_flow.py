65c4c8938ab8e78d37d917eb6f0e37115eaced30
Eric Hare <eric@omnianalytics.io>
2022-02-28 14:06:36 -0700

    Update daisi_flow.py

--- daisi_flow.py
+++ daisi_flow.py
@@ -25,7 +25,7 @@ ms = MapStack(nx, ny)
 for i in range(10):
     ms.add_layer(np.random.rand(nx, ny))
 
-status, ms = obj_daisi.compute(func="compute", map_stack=ms, map=np.random.rand(nx, ny))[0]
+status, ms = obj_daisi.compute(func="compute", map_stack=ms, map=np.random.rand(nx, ny))
 
 print(status, ms.nb_layers)
 
